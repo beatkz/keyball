561e2a048bd5afe6c414894dfc3642e1020bf38a
Keiji Momose <keiji.momose@soliton.co.jp>
2026-06-12 11:54:28 +0900

    keyball61のRGBライトカット版であるishキーマップを追加。

--- build.sh
+++ build.sh
@@ -5,11 +5,15 @@ JOBS=$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g') + 1))
 #qmk compile -j 7 -kb keyball/keyball44 -km via
 qmk compile -j $JOBS -kb keyball/keyball44 -km via
 qmk compile -j $JOBS -kb keyball/keyball61 -km via
+qmk compile -j $JOBS -kb keyball/keyball61 -km via_ish
 qmk compile -j $JOBS -kb keyball/keyball44 -km nicola_via
 qmk compile -j $JOBS -kb keyball/keyball61 -km nicola_via
+qmk compile -j $JOBS -kb keyball/keyball61 -km nicola_via_ish
 
 mkdir -p tmp
 cp ../__qmk__/keyball_keyball44_via.hex tmp/keyball44_via.hex
 cp ../__qmk__/keyball_keyball61_via.hex tmp/keyball61_via.hex
+cp ../__qmk__/keyball_keyball61_via_ish.hex tmp/keyball61_via_ish.hex
 cp ../__qmk__/keyball_keyball44_nicola_via.hex tmp/keyball44_nicola_via.hex
 cp ../__qmk__/keyball_keyball61_nicola_via.hex tmp/keyball61_nicola_via.hex
+cp ../__qmk__/keyball_keyball61_nicola_via_ish.hex tmp/keyball61_nicola_via_ish.hex
